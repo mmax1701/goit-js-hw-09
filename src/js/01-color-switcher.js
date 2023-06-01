42279ad6014eed766b18d03ff2e917ef80b5f940
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-06-01 22:29:05 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,20 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timer = null;
+
+start.addEventListener('click', () => {
+    timer = setInterval(() => {
+body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+    
+})
+
+stop.addEventListener('click', () => {
+    clearInterval(timer);
+})
