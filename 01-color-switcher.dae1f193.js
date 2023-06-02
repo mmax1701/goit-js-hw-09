bba885bf66b1eb13d6c906af559c51ea4cffc011
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-06-02 07:26:55 +0000

    Deploying to gh-pages from @ mmax1701/goit-js-hw-09@35ccaecf88970cc701916072f74da691eb32e126 🚀

--- 01-color-switcher.dae1f193.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r=null;t.addEventListener("click",(()=>{r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{clearInterval(r)}));
-//# sourceMappingURL=01-color-switcher.dae1f193.js.map
