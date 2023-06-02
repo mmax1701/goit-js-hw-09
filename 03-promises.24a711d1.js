3cf996cd90f258106d5df6cb58cdad32d9d0002e
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-06-02 18:55:29 +0000

    Deploying to gh-pages from @ mmax1701/goit-js-hw-09@bcf97e10eb76e1b4b8e96a2fcb65616ce6687c7d 🚀

---
+++ 03-promises.24a711d1.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("delay"),t=document.querySelector("step"),u=document.querySelector("amount"),r=document.querySelector("button");Number(e.value),Number(t.value),Number(u.value);r.addEventListener("click",(e=>{e.preventDefault()}));
+//# sourceMappingURL=03-promises.24a711d1.js.map
---
+++ 03-promises.d36a219d.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("delay"),u=document.querySelector("step"),t=document.querySelector("amount"),r=document.querySelector("button");Number(e.value),Number(u.value),Number(t.value);r.addEventListener("click",(function(e){e.preventDefault()}))}();
+//# sourceMappingURL=03-promises.d36a219d.js.map
