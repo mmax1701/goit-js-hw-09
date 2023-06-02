bcf97e10eb76e1b4b8e96a2fcb65616ce6687c7d
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2023-06-02 21:54:50 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,18 @@
+const delayEl = document.querySelector('delay');
+const stepEl = document.querySelector('step');
+const amountEl = document.querySelector('amount');
+const btnEl = document.querySelector('button');
+
+const delay = Number(delayEl.value);
+const step = Number(stepEl.value);
+const amount = Number(amountEl.value);
+
+btnEl.addEventListener('click', (event) => {
+  event.preventDefault();
+})
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
